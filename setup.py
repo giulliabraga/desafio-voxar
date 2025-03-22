07169311ee331ab690bf23df2eba4a6f69b8ad64
Giullia Braga <giubdam@gmail.com>
2025-03-22 11:44:39 -0300

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='3D object reconstruction from 2D images',
+    author='Giullia Braga',
+    license='MIT',
+)
